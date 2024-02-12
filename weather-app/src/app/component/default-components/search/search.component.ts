a16165e820f386265f608ca0ff75f5e7f9153a40
ShadowNightCoder <barbar0608@gmail.com>
2024-02-13 00:48:39 +0200

    worked on some css and changeing component places

--- weather-app/src/app/component/default-components/search/search.component.ts
+++ weather-app/src/app/component/default-components/search/search.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
+  timeStatus = 'night'
+  src = ''
+  @Output() citySelected = new EventEmitter<string>();
 
+  ngOnInit(): void {
+      if(this.timeStatus === 'day'){
+        this.src='./../../../../assets/img/day-night/day.jpg'
+      }
+      else{
+        this.src='./../../../../assets/img/day-night/moonnight.jpg'
+      }
+  }
+
+  onSubmit(CityInput: string) {
+    this.citySelected.emit(CityInput); // Emit the city data
+  }
+
+  
+  
+  
 }
