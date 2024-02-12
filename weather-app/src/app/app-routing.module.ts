acb6864a647d268d88630e70a5da90a965f5f327
ShadowNightCoder <barbar0608@gmail.com>
2024-02-12 14:16:52 +0200

    added header now working on main and the two other pages

--- weather-app/src/app/app-routing.module.ts
+++ weather-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WeatherComponent } from './component/pages/weather/weather.component';
+import { FavoritesComponent } from './component/pages/favorites/favorites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WeatherComponent },
+  { path: 'Home', component: WeatherComponent },
+  { path: 'Favorit-city', component: FavoritesComponent },
+  { path: '**', redirectTo: 'Home' }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- weather-app/src/app/app.module.ts
+++ weather-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { WeatherComponent } from './component/pages/weather/weather.component';
+import { FavoritesComponent } from './component/pages/favorites/favorites.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    WeatherComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
