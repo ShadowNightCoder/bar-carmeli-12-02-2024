90e3a7237cfbb5a0aa59a4c31f277de4f8e80b86
ShadowNightCoder <barbar0608@gmail.com>
2024-02-13 15:44:55 +0200

    fixing css need more work

--- weather-app/src/app/component/default-components/card/card.component.ts
+++ weather-app/src/app/component/default-components/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { weatherForFiveDays } from 'src/app/interface/weatherarray';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent {
+  @Input() weatherForDay!: weatherForFiveDays;
+
 
 }
